c767f1e352743d1ace3f2dbd4433ec34a7e68d5f
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2026-01-29 20:26:57 +0700

    Init project

---
+++ mini-ecommerce-platform-server/src/main/java/com/nhannh/mini_ecommerce_platform/MiniEcommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.nhannh.mini_ecommerce_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEcommercePlatformApplication.class, args);
+	}
+
+}
---
+++ mini-ecommerce-platform-server/src/test/java/com/nhannh/mini_ecommerce_platform/MiniEcommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhannh.mini_ecommerce_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
