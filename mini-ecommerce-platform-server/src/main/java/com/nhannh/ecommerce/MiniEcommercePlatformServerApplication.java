8a7a7f0aba417813963140a71bf9455883739f26
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2026-01-31 21:08:50 +0700

    Story MECP-2: Integrate liquibase with the application

--- mini-ecommerce-platform-server/src/main/java/com/nhannh/ecommerce/MiniEcommercePlatformServerApplication.java
+++ mini-ecommerce-platform-server/src/main/java/com/nhannh/ecommerce/MiniEcommercePlatformServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiniEcommercePlatformApplication {
+public class MiniEcommercePlatformServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MiniEcommercePlatformApplication.class, args);
+        SpringApplication.run(MiniEcommercePlatformServerApplication.class, args);
     }
 
 }
