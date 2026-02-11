2583474ea8390ef3d76925d2a994aa5b77265cd1
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2026-02-11 17:49:31 +0700

    Story MECP-19: Create checkout API endpoint

--- mini-ecommerce-platform-server/src/main/resources/db/changelog/ddl/order-items-changelog.sql
+++ mini-ecommerce-platform-server/src/main/resources/db/changelog/ddl/order-items-changelog.sql
@@ -8,12 +8,12 @@ CREATE TABLE order_items (
     product_name VARCHAR(255) NOT NULL,
     product_price NUMERIC(19,2) NOT NULL,
     quantity INTEGER NOT NULL,
-    subtotal NUMERIC(19,2) NOT NULL,
+    sub_total NUMERIC(19,2) NOT NULL,
     created_on TIMESTAMP NOT NULL,
     modified_on TIMESTAMP NOT NULL,
     CONSTRAINT fk_order_items_order FOREIGN KEY (order_id) REFERENCES orders(id),
     CONSTRAINT ck_order_items_quantity CHECK (quantity > 0),
-    CONSTRAINT ck_order_items_subtotal CHECK (subtotal >= 0)
+    CONSTRAINT ck_order_items_subtotal CHECK (sub_total >= 0)
 );
 
 CREATE INDEX idx_order_items_order_id ON order_items (order_id);
