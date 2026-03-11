f08f0738a386fce212c579fce08c92ed86c7d333
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2026-03-11 10:25:50 +0700

    Story MECP-30: Create simple home page

--- mini-ecommerce-platform-ui/src/App.jsx
+++ mini-ecommerce-platform-ui/src/App.jsx
@@ -1,21 +1,32 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import RegisterPage from "./pages/RegisterPage.jsx";
-import SignupForm from "./components/SignupForm.jsx";
+import {Link, Route, Routes} from 'react-router-dom';
+
+function HomePage() {
+    return (
+        <h1>Home Page</h1>
+    );
+}
+
+function AboutPage() {
+    return (
+        <h1>About Page</h1>
+    );
+}
 
 function App() {
     return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/register" element={<RegisterPage/>}/>
-                </Routes>
-            </BrowserRouter>
-            <div>
-                <SignupForm/>
-            </div>
-        </>
-    )
+        <div>
+            <nav style={{display: "flex", gap: "1rem", marginBottom: "5px"}}>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/about"}>About</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="*" element={<h1>404 Not Found</h1>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App
